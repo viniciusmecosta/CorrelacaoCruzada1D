b6e626b0d0de75aaf311fbd9b5d738c6431c8d3f
viniciusmecosta <costavinicius315@gmail.com>
2026-04-22 19:00:13 -0300

    refactor: remover ponteiros

--- src/cross_correlation.c
+++ src/cross_correlation.c
@@ -4,42 +4,45 @@ void calcular_correlacao_cruzada(const float x[], const float y[], float resulta
     int i;
     int j;
     int deslocamento;
+    int amostras_sobrepostas;
+    int inicio_x;
+    int inicio_y;
     float soma;
-
-    const float *ptr_x;
-    const float *ptr_y;
 
     for (i = 0; i < SIGNAL_SIZE - 1; i++) {
         deslocamento = (SIGNAL_SIZE - 1) - i;
+        amostras_sobrepostas = i + 1;
+        inicio_x = 0;
+        inicio_y = deslocamento;
         soma = 0.0f;
-        ptr_x = x;
-        ptr_y = y + deslocamento;
 
-        for (j = 0; j <= i; j++) {
-            soma += (*ptr_x++) * (*ptr_y++);
+        for (j = 0; j < amostras_sobrepostas; j++) {
+            soma += x[inicio_x + j] * y[inicio_y + j];
         }
 
         resultado[i] = soma;
     }
 
+    amostras_sobrepostas = SIGNAL_SIZE;
+    inicio_x = 0;
+    inicio_y = 0;
     soma = 0.0f;
-    ptr_x = x;
-    ptr_y = y;
 
-    for (j = 0; j < SIGNAL_SIZE; j++) {
-        soma += (*ptr_x++) * (*ptr_y++);
+    for (j = 0; j < amostras_sobrepostas; j++) {
+        soma += x[inicio_x + j] * y[inicio_y + j];
     }
 
     resultado[SIGNAL_SIZE - 1] = soma;
 
     for (i = SIGNAL_SIZE; i < CORRELATION_SIZE; i++) {
         deslocamento = i - (SIGNAL_SIZE - 1);
+        amostras_sobrepostas = SIGNAL_SIZE - deslocamento;
+        inicio_x = deslocamento;
+        inicio_y = 0;
         soma = 0.0f;
-        ptr_x = x + deslocamento;
-        ptr_y = y;
 
-        for (j = deslocamento; j < SIGNAL_SIZE; j++) {
-            soma += (*ptr_x++) * (*ptr_y++);
+        for (j = 0; j < amostras_sobrepostas; j++) {
+            soma += x[inicio_x + j] * y[inicio_y + j];
         }
 
         resultado[i] = soma;
